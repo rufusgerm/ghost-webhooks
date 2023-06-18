7cc71644572ad9ba3628191f2176007d15265848
Jeremy Bergevin <bergevinjd@gmail.com>
2023-06-18 11:53:05 -0700

    Reconfigured the entire server logic to be more decoupled and readable.

--- index.ts
+++ index.ts
@@ -1,34 +1,36 @@
 // import express and generate basic app with one route of /hooks
 import express from 'express';
-import mysql from 'mysql';
+import MysqlClientProvider from './mysql/mysqlClientProvider';
 
 const app = express();
 
-// create mysql client with a host that references the docker service and gets the user, password, and db name from the env
-const client = mysql.createConnection({
-  host: 'mysql',
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
-});
+const mysql = new MysqlClientProvider();
+// const batchEmailSender = BatchEmailSenderFactory.createBatchEmailSender('process.env.EMAIL_PROVIDER');
+const batchEmailSender = BatchEmailSenderFactory.createBatchEmailSender('postmark');
   
 
-
 app.get('/', (req, res) => {
-
     res.send('Hello, what\'s up!');
 });
 
 app.post('/hooks', (req, res) => {
   // get the body of the request and parse it as JSON
-  const body = JSON.parse(req.body);
-  // get the newsletter id from the object
-  const newsletterId = body.id;
+  const postData = JSON.parse(req.body);
+  console.log(`postData: ${postData}`);
+  // get the post id from the object
+  const postId = postData.current.id;
+  console.log(`postId: ${postId}`);
   // query the database for member emails with said newsletter id
-  // parse the return into an array of emails
-  // send the emails using the batch sender interface
-  // ensure emails were sent correctly, if not catalog which weren't sent
-  // log the results of the send
+  const emails: string[] = mysql.getEmailsByPostId(postId);
+  const newsletterName: string = mysql.getNewsletterNameByPostId(postId);
+  
+  const { failureCount, failureEmails } = batchEmailSender
+    .send(emails, newsletterName, 'New Post!');
+  
+  console.log(`${failureCount} emails failed to send`);
+  console.log(`Failed emails list: ${failureEmails}`);
+
+  res.status(200).send('OK');
 });
 
 
